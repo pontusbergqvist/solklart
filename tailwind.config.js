69fc1c6a5bcf31a7601569833d89b473ec2e19b5
Pontus <pontusbergqvist97@gmail.com>
2022-09-06 20:19:06 +0200

    First draft

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontSize: {
+        h2: '35px',
+        h3: '25px',
+        h4: '20px'
+      }
+    },
+  },
+  plugins: [],
+};
