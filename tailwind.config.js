0f3938c2b7d1972dbb5f8888f5a2b46b32fec88b
Pontus <pontusbergqvist97@gmail.com>
2022-09-17 23:26:30 +0200

    added fonts and fixed details

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
         h2: '35px',
         h3: '25px',
         h4: '16px'
+      },
+      fontFamily: {
+        mono: ['Dank Mono', 'sans-serif'],
+        body: ['Tahoma', 'sans-serif'],
+        serif: ['Cambria', 'serif']
       }
     },
   },
